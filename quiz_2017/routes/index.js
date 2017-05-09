620e4d344507a11a11da20b79ddd16f9dfec5052
Natalia Torvisco Oto <natalia.torvisco.oto@alumnos.upm.es>
2017-05-09 17:17:44 +0200

    modificacion1

---
+++ quiz_2017/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+  res.render('index', { title: 'Quiz'});
+});
+
+// Pagina de creditos
+
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
+module.exports = router;
+
